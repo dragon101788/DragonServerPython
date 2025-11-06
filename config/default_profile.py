3a46e1b04c053da81563695acdfed94e2bde1334
dragon <dragon101788@qq.com>
2025-11-06 17:09:34 +0800

    refactor: 重构项目结构，将WebServer.py移动到src目录
    - 删除根目录下的WebServer.py文件
    - 在src目录下创建新的WebServer.py
    - 更新server_config.py中的app路径引用
    - 清理配置文件中的冗余字段

--- config/default_profile.py
+++ config/default_profile.py
@@ -1,2 +1,2 @@
 # 这是 ChatAPI 的配置文件
-config = {'password': 'default_user', 'nickname': 'default_user', 'description': '我是一个新用户，请尽快设置我的描述', 'type': 'user', 'WebSession': '/chatroom/NormalSessionWeb.html', 'role': ['User']}
+config = {'password': 'default_user', 'nickname': 'default_user', 'description': '我是一个新用户，请尽快设置我的描述', 'type': 'user', 'role': ['User'], 'WebSession': '/chatroom/NormalSessionWeb.html'}
--- server_config.py
+++ server_config.py
@@ -17,7 +17,7 @@ server_config = config.PythonConfig("config/server_config.py",default_config={
         {
             "type" : "uvicorn",#服务类型uvicorn
             "enabled" : True, #是否启用，默认True
-            "app" : "WebServer:app",#app的路径，格式为 module:app，例如 WebServer:app，WebServer是模块名，app是应用实例名
+            "app" : "src.WebServer:app",#app的路径，格式为 module:app，例如 WebServer:app，WebServer是模块名，app是应用实例名
             "config" :{   
                 "host" : "0.0.0.0",#主机地址，默认0.0.0.0
                 "port" : 8443, #端口号，默认8443
@@ -28,7 +28,7 @@ server_config = config.PythonConfig("config/server_config.py",default_config={
         {
             "type" : "uvicorn",#服务类型uvicorn
             "enabled" : True, #是否启用，默认True
-            "app" : "WebServer:app",
+            "app" : "src.WebServer:app",
             "config" :{
                 "host" : "0.0.0.0",
                 "port" : 8800,
