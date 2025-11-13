5b365b874341a7c3f36ba15d413e82c9d1aa072b
dragon <dragon101788@qq.com>
2025-11-13 17:21:57 +0800

    fix: 修正配置文件中服务器名称与端口号不一致的问题
    将配置中的DragonServer8803改为DragonServer8800以匹配实际使用的端口号8800

--- config/launcher.py
+++ config/launcher.py
@@ -1,2 +1,2 @@
 # 这是 launcher.py 的配置文件
-config = {'DragonServer8803': {'name': 'DragonServer8803', 'path': 'D:/IMPORTANT/python/DragonServer/DragonServer.exe', 'args': '--port 8800', 'cwd': 'D:/IMPORTANT/python/DragonServer', 'startup_with_windows': False, 'try_admin': False}}
+config = {'DragonServer8800': {'name': 'DragonServer8800', 'path': 'D:/IMPORTANT/python/DragonServer/DragonServer.exe', 'args': '--port 8800', 'cwd': 'D:/IMPORTANT/python/DragonServer', 'startup_with_windows': False, 'try_admin': False}}
